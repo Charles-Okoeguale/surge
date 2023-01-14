c3795da1688c5cd6648c89d1e5b2a5eaaa9179d6
Charles1403 <osehaeguale@gmail.com>
2023-01-14 03:39:30 +0100

    add username auth flow

---
+++ frontend/src/pages/Authflow/Username/index.jsx
@@ -0,0 +1,27 @@
+import { Box,Button,Flex, Input, Text } from '@chakra-ui/react';
+import { LogoSVG } from '../../../components/Svg';
+
+function Username() {
+  return (
+        <Flex w='100%'>
+        <Box w='45%' backgroundColor='#111827' height='100vh'>
+            <Box mx={50} my={5} w='6.1em' h='3.1em'>
+                <LogoSVG/>
+            </Box>
+            <Box my={220} mx={50}>
+            <Text my={3} color='white' fontWeight={700} fontSize='2.5em' letterSpacing='-0.05em' lineHeight='1em'>
+               Set Username
+            </Text>
+            <Text color='rgba(255, 255, 255, 0.4)' fontSize='0.8em' lineHeight='1em' letterSpacing='-0.03em'>
+                Setup your username of your liking e.g Charles1403
+            </Text>
+            <Input  w='100%' h='4em' marginTop={10} background='rgba(255, 255, 255, 0.05)' color='white' borderRadius='1em' placeholder='Enter Username'/>
+            <Button  w='100%' h='4em' marginTop='1.4em' background='rgba(16, 185, 112, 0.1);' color='#10B970' borderRadius='1em'>NEXT</Button>
+            </Box>
+        </Box>
+        <Box w='55%' backgroundColor='#172135' height='100vh'/>
+    </Flex>
+  )
+}
+
+export default Username
