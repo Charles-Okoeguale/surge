d0030a26f17477b60f41ccf1ccb64eed0aa7f7c5
dunsin <ajibabidunsin@gmail.com>
2023-02-08 09:02:59 +0100

    fixed the signup and username pages

--- frontend/src/pages/Authflow/Username/index.tsx
+++ frontend/src/pages/Authflow/Username/index.tsx
@@ -1,28 +1,101 @@
-// import { Box,Button,Flex, Input, Text } from '@mui/material';
-// import { LogoSVG } from '../../../components/Svg';
-
-// function Username() {
-//   return (
-//         <Flex w='100%'>
-//         <Box w='45%' backgroundColor='#111827' height='100vh'>
-//             <Box mx={50} my={5} w='6.1em' h='3.1em'>
-//                 <LogoSVG/>
-//             </Box>
-//             <Box my={220} mx={50}>
-//             <Text my={3} color='white' fontWeight={700} fontSize='2.5em' letterSpacing='-0.05em' lineHeight='1em'>
-//                Set Username
-//             </Text>
-//             <Text color='rgba(255, 255, 255, 0.4)' fontSize='0.8em' lineHeight='1em' letterSpacing='-0.03em'>
-//                 Setup your username of your liking e.g Charles1403
-//             </Text>
-//             <Input  w='100%' h='4em' marginTop={10} background='rgba(255, 255, 255, 0.05)' color='white' borderRadius='1em' placeholder='Enter Username'/>
-//             <Button  w='100%' h='4em' marginTop='1.4em' background='rgba(16, 185, 112, 0.1);' color='#10B970' borderRadius='1em'>NEXT</Button>
-//             </Box>
-//         </Box>
-//         <Box w='55%' backgroundColor='#172135' height='100vh'/>
-//     </Flex>
-//   )
-// }
-
-// export default Username
 import React from "react";
+import { Box, Typography, OutlinedInput} from "@mui/material"
+import { createStyles, makeStyles } from "@mui/styles";
+import {  LogoSVG } from '../../../components/Svg';
+import Button from "@mui/material/Button";
+import { Theme } from "@mui/material";
+import { color } from "@mui/system";
+
+
+function Username() {
+
+    const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+        <Box className={classes.container}>
+             <Box className={classes.logo}>
+                <LogoSVG/>
+             </Box>
+            <Box className={classes.content}>
+            <Box>
+                <Typography 
+                variant="h4" 
+                style={{
+                color: "white",
+                fontWeight: "700"
+                }}>
+                    Set Username
+                </Typography>
+                <Typography 
+                variant="body2"
+                style={{
+                color: "rgba(255, 255, 255, 0.4)"
+                }}>
+                Setup your username of your liking e.g gegeoflagos
+                </Typography>
+            </Box>
+            <OutlinedInput className={classes.input} placeholder="Enter Username"/>
+            <Button className={classes.button}>Next</Button>
+            </Box>
+               
+        </Box>
+        <Box className={classes.aside}/>
+    </Box>
+   
+  )
+}
+
+export default Username;
+
+const useStyles = makeStyles((theme: Theme)=>
+    ({
+    root:{
+        display: "flex",
+        margin: "auto",
+        width: "100%",
+        height: '100vh',
+        background: "#111827"
+    },
+
+    logo:{
+        marginLeft: "2rem",
+        width: "6.1em",
+        height: "3.1em"
+    },
+
+    container:{
+        width: "50%",
+    },
+
+    content:{
+       position: "relative",
+       top: "200px",
+       display: "flex",
+       flexDirection: "column",
+       alignItems: "center",
+       gap:'20px',
+    },
+
+    button:{
+        width: "520px",
+        height: "60px",
+        background: "rgba(16, 185, 112, 0.1)",
+        borderRadius: "16px",
+        color: "#10B970"
+    },
+
+    input:{
+      width: '32em',
+      height: '3.5em',
+      color: 'white',
+      borderRadius: "16px",
+      background: "rgba(255, 255, 255, 0.05)"
+    },
+
+    aside:{
+        width: "50%",
+        backgroundColor:"#172135"
+    }
+
+}))
