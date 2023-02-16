b3e6670327013b4be8b96043c28538944caf9e04
Charles1403 <osehaeguale@gmail.com>
2023-02-16 16:08:33 +0100

    create server connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,30 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(cors())
 app.use(express.json());
+app.use(cookieParser());
+mongoose.set('strictQuery', false)
+
+const DBURI = "mongodb+srv://Charles-Eguale:14032001@cluster0.lxhxkxl.mongodb.net/?retryWrites=true&w=majority"
 
 app.post("/signup", async (req : Request, res: Response, ) => {
-   console.log(req.body)
+    console.log(req.body)
 })
 
-app.listen("4000", () => console.log('server running'))
+// async function connect () {
+//     try {
+//         await mongoose.connect(DBURI)
+//         console.log("connected to database")
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// connect()
+
+app.listen(8000, () => console.log("server running on port 8000"))
+
